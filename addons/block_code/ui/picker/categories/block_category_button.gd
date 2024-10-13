17de7043b0425ddfb200eacfa67249d9ed1db07e
Daze <pating.wolfe@yahoo.com>
2024-10-13 03:51:39 +0000

    Unique icons

--- addons/block_code/ui/picker/categories/block_category_button.gd
+++ addons/block_code/ui/picker/categories/block_category_button.gd
@@ -7,7 +7,7 @@ const Util = preload("res://addons/block_code/ui/util.gd")
 signal selected
 
 var category: BlockCategory
-var gui = EditorInterface.get_base_control()
+#var gui = EditorInterface.get_base_control()
 
 @onready var _panel := %Panel
 @onready var _label := %Label
@@ -18,6 +18,8 @@ func _ready():
 		category = BlockCategory.new("Example", Color.RED)
 
 	if not Util.node_is_part_of_edited_scene(self):
+		var texture = load("res://addons/block_code/ui/picker/categories/category_icons/" + category.icon + ".svg")#Texture2D.new()
+		_panel.texture = texture#gui.get_theme_icon(category.icon, "EditorIcons")
 		_panel.modulate = category.color
 
 	_label.text = category.name
